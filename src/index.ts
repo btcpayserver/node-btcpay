ebc5fb59c8de19ca2ce5b2749ed8003d0c3830bb
Christoph Ott <christoph.ott@lean-coders.at>
2019-04-19 18:50:54 +0200

    going on with ts refactoring

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as cryptography from './core/cryptography';
+import * as BTCPayClient from './core/client';
+
+exports.crypto = cryptography.Cryptography;
+exports.BTCPayClient = BTCPayClient.BTCPayClient;
