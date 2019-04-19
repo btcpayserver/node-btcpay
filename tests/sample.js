dd8e815c697f9ef23e18c1f71029f5056c37455c
Christoph Ott <christoph.ott@lean-coders.at>
2019-04-19 18:59:26 +0200

    adde sample code

---
+++ tests/sample.js
@@ -0,0 +1,9 @@
+const btcpay = require('../dist');
+const keypair = btcpay.crypto.load_keypair(new Buffer.from('', 'hex'));
+
+const client = new btcpay.BTCPayClient('', keypair, { merchant: '' });
+
+client
+  .get_rates('BTC_USD', '')
+  .then((rates) => console.log(rates))
+  .catch((err) => console.log(err));
