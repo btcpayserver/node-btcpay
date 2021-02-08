60ef5b938a9cd012b9c4f1e41afe6f66fb52c7fd
Jonathan Underwood <junderwood@bitcoinbank.co.jp>
2021-02-08 10:40:17 +0900

    100% Coverage (#56)

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,5 +3,7 @@ import * as btcpay from '../src/index';
 describe('btcpay.index', () => {
   it('should import', () => {
     expect(btcpay).toBeDefined();
+    expect(btcpay.crypto).toBeDefined();
+    expect(btcpay.BTCPayClient).toBeDefined();
   });
 });
